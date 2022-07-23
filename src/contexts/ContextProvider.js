c15c1587de7978faa6b9bdbc4900a480b5d3e6d4
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-07-23 19:22:05 +0200

    Adding functions relative to Navbar

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsCliked] = useState(initialState);
+
+    const handleClick = (clicked) => {
+        setIsCliked({ ...initialState, [clicked]: true });
+    }
+
+
+    return (
+        <StateContext.Provider value={{
+            activeMenu,
+            setActiveMenu,
+            isClicked,
+            setIsCliked,
+            handleClick
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
