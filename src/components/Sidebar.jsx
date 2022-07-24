844a1a23ada1790c1aae4a1b22ef0379f9d19b6b
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-07-24 12:36:55 +0200

    Manage screenSize

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,14 @@ import { links } from '../data/dummy'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  }
+
   const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
@@ -22,7 +29,7 @@ const Sidebar = () => {
             <SiShopware /> <span>Shoppy</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
-            <button type="button" onClick={() => setActiveMenu(!activeMenu)}
+            <button type="button" onClick={handleCloseSideBar}
             className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block">
               <MdOutlineCancel />
             </button>
@@ -38,7 +45,7 @@ const Sidebar = () => {
                 <NavLink
                   to={`/${link.name}`}
                   key={link.name}
-                  onClick={() => {}}
+                  onClick={handleCloseSideBar}
                   className={({ isActive }) => (isActive ? activeLink : normalLink)}
                 >
                   {link.icon}
