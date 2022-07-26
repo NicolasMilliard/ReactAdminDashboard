13bb6935389a022ed588f1e92dffcb76bc7e9960
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-07-26 15:43:31 +0200

    Init commit

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, CandleSeries, Tooltip, DateTime, Zoom, Logarithmic, Crosshair } from '@syncfusion/ej2-react-charts'
+
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+import { ChartsHeader } from '../../components'
+
+const date1 = new Date('2017, 1, 1');
+
+function filterValue(value) {
+  if(value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+
+const returnValue = financialChartData.filter(filterValue);
 
 const Financial = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>Financial</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded3">
+      <ChartsHeader category="Financial" title="AAPLE History" />
+      <div className="w-full">
+        <ChartComponent
+          id="financial-chart"
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 } }}
+          background={currentMode === 'Dark' ? "#33373e" : "#fff"}
+        >
+          <Inject services={[CandleSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName="x"
+              yName="low"
+              name="Apple Inc"
+              type="Candle"
+              enableSolidCandles="true"
+              bullFillColor= "#f23645"
+              bearFillColor="#089981"
+              low="low"
+              high="high"
+              open="open"
+              close="close"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
   )
 }
 
