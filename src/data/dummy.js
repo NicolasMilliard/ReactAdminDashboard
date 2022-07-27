26ad4fd1e5ec3fde56f44acbc7bb7ca61bc1dc1c
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-07-27 11:22:26 +0200

    Change FiCreditCard iconColor

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -865,7 +865,7 @@ export const userProfileData = [
     icon: <FiCreditCard />,
     title: 'My Tasks',
     desc: 'To-do and Daily Tasks',
-    iconColor: 'rgb(255, 244, 229)',
+    iconColor: 'rgb(230, 194, 104)',
     iconBg: 'rgb(254, 201, 15)',
   },
 ];
