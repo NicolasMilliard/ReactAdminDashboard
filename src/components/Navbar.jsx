ac49732cbfe512044f1c9180bfa768d5f1371fc7
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-07-27 11:22:05 +0200

    Adding handleClick for userProfile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,11 +48,11 @@ const Navbar = () => {
         <NavButton title="Chat" dotColor="#03c9d7" customFunc={() => handleClick("chat")} color={currentColor} icon={<BsChatLeft />} />
         <NavButton title="Notifications" dotColor="#03c9d7" customFunc={() => handleClick("notification")} color={currentColor} icon={<RiNotification3Line />} />
         <TooltipComponent content="Profile" position="BottomCenter">
-          <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => {}}>
+          <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => handleClick("userProfile")}>
             <img src={avatar} className="rounded-full w-8 h-8" />
             <p>
               <span className="text-gray-400 text-14">Hi, </span> {' '}
-              <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
+              <span className="text-gray-400 font-bold ml-1 text-14">Nicolas</span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
